0369dd7d17cd0315630d07a2602b7d978861f8eb
mynameisguo1 <1060816825@qq.com>
2016-12-16 09:44:48 +0800

    Add files via upload

--- public/js/3D_yinyue.js
+++ public/js/3D_yinyue.js
@@ -23,37 +23,19 @@ $(function() {
             b = parseInt(a / 17);
         }
     }, 2000);
-    //ת��Ч��
-    var move = false;//�ƶ����
-    var _x, _y;//_x,_y�����ؼ����Ͻǵ����λ��
-    $("body").mousedown(function (e) {
-        move = true;
-        $('body').css("cursor","default");
-        _x = e.pageX - parseInt($('body').offset().left);
-        _y = e.pageY - parseInt($('body').offset().top);
-    });
-    $(document).mousemove(function (e) {
-        if (move) {
-            var x = e.pageX - _x;//�ؼ����Ͻǵ���Ļ���Ͻǵ����λ��
-            var y = e.pageY - _y;
-            $(".rong_qi").css({'transform': 'rotateY('+ x +'deg) rotateX('+ y +'deg)'});
-            $(".big_rong_qi").css({'transform': 'rotateY('+ x +'deg) rotateX('+ y +'deg)'});
-        }
-    }).mouseup(function () {
-        move = false;
-    });
     //���Բ��
+    var biao_a = [{'1':'阿兰','2':'千古','3':'A'},{'1':'魏新雨','2':'恋人心','3':'B'},{'1':'Trouble Maker','2':'Trouble Maker','3':'C'},{'1':'李晓杰、陈星、程巍、牛朝阳','2':'美人酒','3':'D'},{'1':'颗粒','2':'柔软的光','3':'E'},{'1':'赵真、百灵','2':'好运连连','3':'F'},{'1':'赵真','2':'福门开 好运来','3':'G'},{'1':'黑龙、胡铁','2':'友情岁月','3':'H'},{'1':'徐浩、朱元冰','2':'跟着太阳走','3':'I'},{'1':'星弟','2':'制造浪漫','3':'J'},{'1':'贯诗钦','2':'你明知道','3':'K'},{'1':'贺美琦','2':'成长的约定','3':'L'},{'1':'余枫','2':'爱无以复加','3':'M'},{'1':'江涛','2':'舍不得','3':'N'},{'1':'刘程浩','2':'我只要你一个选择','3':'O'},{'1':'Alan Walker(艾伦·沃克)','2':'Alone','3':'P'},{'1':'周杰伦','2':'蜗牛','3':'Q'},{'1':'常石磊','2':'我我','3':'R'},{'1':'BIGBANG(빅뱅)','2':'BIGBANG 最新数字专辑QQ音乐震撼独家发行','3':'S'},{'1':'ACEMAX-RED','2':'MAXY（火力全开）','3':'T'},{'1':'','2':''},{'1':'','2':''},{'1':'','2':''},{'1':'','2':''},{'1':'','2':''},{'1':'','2':''},{'1':'','2':''},{'1':'','2':''},{'1':'','2':''},{'1':'','2':''},{'1':'','2':''},{'1':'','2':''},{'1':'','2':''},{'1':'','2':''},{'1':'','2':''},{'1':'','2':''},{'1':'','2':''},{'1':'','2':''},{'1':'','2':''},{'1':'','2':''}];
+    var biao_b = [{'0':'A'},{'0':'B'},{'0':'C'},{'0':'D'},{'0':'E'},{'0':'F'},{'0':'G'},{'0':'H'},{'0':'I'},{'0':'J'},{'0':'K'},{'0':'L'},{'0':'M'},{'0':'N'},{'0':'O'},{'0':'P'},{'0':'Q'},{'0':'R'},{'0':'S'},{'0':'T'}];
     $('.btn_y').click(function(){
-        var biao_a = [{'0':'A','1':'阿兰','2':'千古'},{'0':'B','1':'魏新雨','2':'恋人心'},{'0':'C','1':'高安、姜楠','2':'梁祝情'},{'0':'D','1':'李晓杰、陈星、程巍、牛朝阳','2':'美人酒'},{'0':'E','1':'颗粒','2':'柔软的光'},{'0':'F','1':'赵真、百灵','2':'好运连连'},{'0':'G','1':'赵真','2':'福门开 好运来'},{'0':'H','1':'黑龙、胡铁','2':'友情岁月'},{'0':'I','1':'徐浩、朱元冰','2':'跟着太阳走'},{'0':'J','1':'星弟','2':'制造浪漫'},{'0':'K','1':'贯诗钦','2':'你明知道'},{'0':'L','1':'贺美琦','2':'成长的约定'},{'0':'M','1':'余枫','2':'爱无以复加'},{'0':'N','1':'江涛','2':'舍不得'},{'0':'O','1':'刘程浩','2':'我只要你一个选择'},{'0':'P','1':'Alan Walker(艾伦·沃克)','2':'Alone'},{'0':'Q','1':'周杰伦','2':'蜗牛'},{'0':'R','1':'常石磊','2':'我我'},{'0':'S','1':'BIGBANG(빅뱅)','2':'BIGBANG 最新数字专辑QQ音乐震撼独家发行'},{'0':'T','1':'ACEMAX-RED','2':'MAXY（火力全开）'}];
         setTimeout(function(){
             var l = $('.div1').length;
             for(var i = 72;i < 81;i++){
                 $('.div1').eq(i).css({'transform':'rotateX(40deg)'});
-                $('.div1').eq(i).html('<p class="p_biaoqian">'+biao_a[(i-72)][0]+'</p><p class="p_biaoqian" title='+biao_a[(i-72)][1]+'>'+biao_a[(i-72)][1]+'</p><p class="p_biaoqian" title='+biao_a[(i-72)][2]+'>'+biao_a[(i-72)][2]+'</p>');
+                $('.div1').eq(i).html('<p class="p_biaoqian">'+biao_b[(i-72)][0]+'</p><p class="p_biaoqian" title='+biao_a[(i-72)][1]+'>'+biao_a[(i-72)][1]+'</p><p class="p_biaoqian" title='+biao_a[(i-72)][2]+'>'+biao_a[(i-72)][2]+'</p>');
             }
             for(var j = 88;j < 99;j++){
                 $('.div1').eq(j).css({'transform':'rotateX(40deg)'});
-                $('.div1').eq(j).html('<p class="p_biaoqian">'+biao_a[(j-79)][0]+'</p><p class="p_biaoqian" title='+biao_a[(j-79)][1]+'>'+biao_a[(j-79)][1]+'</p><p class="p_biaoqian" title='+biao_a[(j-79)][2]+'>'+biao_a[(j-79)][2]+'</p>');
+                $('.div1').eq(j).html('<p class="p_biaoqian">'+biao_b[(j-79)][0]+'</p><p class="p_biaoqian" title='+biao_a[(j-79)][1]+'>'+biao_a[(j-79)][1]+'</p><p class="p_biaoqian" title='+biao_a[(j-79)][2]+'>'+biao_a[(j-79)][2]+'</p>');
             }
         },3000);
         setTimeout(function(){
@@ -73,6 +55,12 @@ $(function() {
         },2000);
         setTimeout(function(){
             $('.div1').eq(4).html('<video class="shi_pin" width="620px" height="350px" src="public/images/nvzongtongA.mkv"></video>');
+            Tu_pian();
+            $('.shi_pin_img').attr('src','public/images/shipin_imgA.jpg');
+            //下面代码是添加一个删除标志
+            $('.div1').eq(31).html('<i class="my_box_a_i my_box_a_fore4"></i>');
+            //下面代码是添加一个添加标志
+            $('.div1').eq(32).html('<i class="my_box_a_i my_box_a_fore2"></i>');
         },5500);
         $('.div1').on('click','.shi_pin',function(){
             var music = document.getElementsByClassName('shi_pin')[0];
@@ -119,26 +107,159 @@ $(function() {
         //        $('.div1').eq(i+j*4).appendTo($('.rong_qi6'));
         //    }
         //}
-
-    });
-    function keyDown(e) {
-        var keycode = e.which;
-        var realkey = String.fromCharCode(e.which);
-        if(keycode == 65){
-            $('.div1').eq(4).html('<video class="shi_pin" width="620px" height="350px" src="public/images/nvzongtongA.mkv"></video>');
+        function keyDown(e) {
+            var keycode = e.which;
+            var realkey = String.fromCharCode(e.which);
+            if(keycode == 65){
+                $('.div1').eq(4).html('<video class="shi_pin" width="620px" height="350px"></video>');
+                $('.shi_pin').attr('src','public/images/nvzongtong'+biao_a[0][3]+'.mkv');
+                Tu_pian();
+                $('.shi_pin_img').attr('src','public/images/shipin_img'+biao_a[0][3]+'.jpg');
+            }
+            if(keycode == 66){
+                $('.div1').eq(4).html('<video class="shi_pin" width="620px" height="350px"></video>');
+                $('.shi_pin').attr('src','public/images/nvzongtong'+biao_a[1][3]+'.mkv');
+                Tu_pian();
+                $('.shi_pin_img').attr('src','public/images/shipin_img'+biao_a[1][3]+'.jpg');
+            }
+            if(keycode == 67){
+                $('.div1').eq(4).html('<video class="shi_pin" width="620px" height="350px"></video>');
+                $('.shi_pin').attr('src','public/images/nvzongtong'+biao_a[2][3]+'.mkv');
+                Tu_pian();
+                $('.shi_pin_img').attr('src','public/images/shipin_img'+biao_a[2][3]+'.jpg');
+            }
+            if(keycode == 32){
+                var music = document.getElementsByClassName('shi_pin')[0];
+                if(music.paused){
+                    music.play();
+                }else{
+                    music.pause();
+                }
+            }
+            //alert('keycode:'+keycode+'符号:'+realkey);
         }
-        if(keycode == 66){
-            $('.div1').eq(4).html('<video class="shi_pin" width="620px" height="350px" src="public/images/nvzongtongB.mkv"></video>');
-        }
-        if(keycode == 32){
-            var music = document.getElementsByClassName('shi_pin')[0];
-            if(music.paused){
-                music.play();
-            }else{
-                music.pause();
+        $('body').keydown(keyDown);
+        function Tu_pian(){
+            for(var i = 0;i<4;i++){
+                $('.div1').eq(i).css('overflow','hidden');
+                $('.div1').eq(i).html('<img class="shi_pin_img" />');
+                $('.shi_pin_img').eq(i).css('left',-i*70+'px');
+            }
+            for(var j = 17;j<21;j++){
+                $('.div1').eq(j).css('overflow','hidden');
+                $('.div1').eq(j).html('<img class="shi_pin_img" />');
+                $('.shi_pin_img').eq(j-13).css({'left':-(j-17)*70+'px','top':'-90px'});
+            }
+            for(var p = 34;p<38;p++){
+                $('.div1').eq(p).css('overflow','hidden');
+                $('.div1').eq(p).html('<img class="shi_pin_img" />');
+                $('.shi_pin_img').eq(p-26).css({'left':-(p-34)*70+'px','top':'-180px'});
+            }
+            for(var u = 51;u<55;u++){
+                $('.div1').eq(u).css('overflow','hidden');
+                $('.div1').eq(u).html('<img class="shi_pin_img" />');
+                $('.shi_pin_img').eq(u-39).css({'left':-(u-51)*70+'px','top':'-270px'});
             }
         }
-        //alert('keycode:'+keycode+'符号:'+realkey);
+
+    });
+    //想写一个滚轮事件，然后查了资料，看的恶心，火狐竟然不一样，怕自己忘了，这次就把兼容一起写上了
+    function addEvent(obj,xEvent,fn) {
+        if(obj.attachEvent){
+            obj.attachEvent('on'+xEvent,fn);
+        }else{
+            obj.addEventListener(xEvent,fn,false);
+        }
     }
-    document.onkeydown = keyDown;
+    //接着利用我们自己封装的函数给div绑定事件，
+    var oBody = $('body')[0];
+    addEvent(oBody,'mousewheel',onMouseWheel);
+    addEvent(oBody,'DOMMouseScroll',onMouseWheel);
+    // 当滚轮事件发生时，执行onMouseWheel这个函数
+    var aa = 0;
+    var move = false;//�ƶ����
+    var _x, _y, x = 0,y = 0;//_x,_y�����ؼ����Ͻǵ����λ��
+    function onMouseWheel(e) {
+        if(e.wheelDelta > 0){
+            aa++;
+        }else{
+            aa--;
+        }
+        if(aa < -9){
+            aa = -9
+        }
+        $('.rong_qi').css('transform','rotateY('+ x +'deg) rotateX('+ y +'deg) scale('+ (0.1*aa+1)+')');
+    }
+    //ת��Ч��
+
+    $("body").mousedown(function (e) {
+        move = true;
+        $('body').css("cursor","default");
+        _x = e.pageX - parseInt($('body').offset().left);
+        _y = e.pageY - parseInt($('body').offset().top);
+    });
+    $(document).mousemove(function (e) {
+        if (move) {
+            x = e.pageX - _x;//�ؼ����Ͻǵ���Ļ���Ͻǵ����λ��
+            y = e.pageY - _y;
+            $(".rong_qi").css({'transform': 'rotateY('+ x +'deg) rotateX('+ y +'deg) scale('+ (0.1*aa+1)+')'});
+            //$(".big_rong_qi").css({'transform': 'rotateY('+ x +'deg) rotateX('+ y +'deg) scale('+ (0.1*aa+1)+')'});
+        }
+    }).mouseup(function () {
+        move = false;
+    });
+    //下面是下面的歌删除的代码，但是因为不能屏蔽body的键盘事件，所以现在放弃，以后在研究
+    $('.div1')[31].onclick = function(){
+        $('.shanchu_box').css('display','block');
+    };
+    $('.shanchu_btn').click(function(){
+        var aInput = $('.shanchu_txt').val();
+        //alert(aInput);
+        var tt = aInput-1;
+        //for(var qq = 0;qq<biao_b.length;qq++){
+        //    if(aInput == biao_b[qq][0]){
+        //        tt = qq;
+        //    }
+        //}
+        biao_a.splice(tt,1);
+        for(var aa = 0;aa<biao_a.length;aa++){
+            biao_a[aa] = biao_a[aa];
+        }
+        var l = $('.div1').length;
+        for(var i = 72;i < 81;i++){
+            $('.div1').eq(i).css({'transform':'rotateX(40deg)'});
+            $('.div1').eq(i).html('<p class="p_biaoqian">'+biao_b[(i-72)][0]+'</p><p class="p_biaoqian" title='+biao_a[(i-72)][1]+'>'+biao_a[(i-72)][1]+'</p><p class="p_biaoqian" title='+biao_a[(i-72)][2]+'>'+biao_a[(i-72)][2]+'</p>');
+        }
+        for(var j = 88;j < 99;j++){
+            $('.div1').eq(j).css({'transform':'rotateX(40deg)'});
+            $('.div1').eq(j).html('<p class="p_biaoqian">'+biao_b[(j-79)][0]+'</p><p class="p_biaoqian" title='+biao_a[(j-79)][1]+'>'+biao_a[(j-79)][1]+'</p><p class="p_biaoqian" title='+biao_a[(j-79)][2]+'>'+biao_a[(j-79)][2]+'</p>');
+        }
+    });
+    $('.shanchu_btn_g').click(function(){
+        $('.shanchu_box').css('display','none');
+    });
+    //下面代码是添加歌曲的
+    $('.div1')[32].onclick = function(){
+        $('.shanchu_box1').css('display','block');
+    };
+    $('.shanchu_btn1').click(function(){
+        var aInput1 = $('.shanchu_txt1').val()-1;
+        var aInput2 = $('.shanchu_txt2').val();
+        var aInput3 = $('.shanchu_txt3').val();
+        biao_a.splice(aInput1,0,{'1':aInput2,'2':aInput3});
+        for(var aa = 0;aa<biao_a.length;aa++){
+            biao_a[aa] = biao_a[aa];
+        }
+        for(var i = 72;i < 81;i++){
+            $('.div1').eq(i).css({'transform':'rotateX(40deg)'});
+            $('.div1').eq(i).html('<p class="p_biaoqian">'+biao_b[(i-72)][0]+'</p><p class="p_biaoqian" title='+biao_a[(i-72)][1]+'>'+biao_a[(i-72)][1]+'</p><p class="p_biaoqian" title='+biao_a[(i-72)][2]+'>'+biao_a[(i-72)][2]+'</p>');
+        }
+        for(var j = 88;j < 99;j++){
+            $('.div1').eq(j).css({'transform':'rotateX(40deg)'});
+            $('.div1').eq(j).html('<p class="p_biaoqian">'+biao_b[(j-79)][0]+'</p><p class="p_biaoqian" title='+biao_a[(j-79)][1]+'>'+biao_a[(j-79)][1]+'</p><p class="p_biaoqian" title='+biao_a[(j-79)][2]+'>'+biao_a[(j-79)][2]+'</p>');
+        }
+    });
+    $('.shanchu_btn_g1').click(function(){
+        $('.shanchu_box1').css('display','none');
+    });
 });
--- public/js/Make_wish.js
+++ public/js/Make_wish.js
@@ -8,4 +8,51 @@ $(function(){
     $('.wish_guanbi').click(function(){
         $('.wish_box').hide();
     });
+    $('.wish_tijiao').click(function(){
+        $('.wish_box').hide();
+        var aDiv = $('<div class="newDiv"></div>');
+        var aDiv_p1 = $('<p class="newDiv_p1"></p>');
+        var aDiv_p2 = $('<p class="newDiv_p2"></p>');
+        var aDiv_p3 = $('<p class="newDiv_p3"></p>');
+        var aDiv_p4 = $('<p class="newDiv_p4"></p>');
+        aDiv_p1.text($('.input1').val());
+        aDiv_p1.appendTo(aDiv);
+        aDiv_p2.text($('.wish_inner').val());
+        aDiv_p2.appendTo(aDiv);
+        aDiv_p3.text(show());
+        aDiv_p3.appendTo(aDiv);
+        aDiv_p4.appendTo(aDiv);
+        aDiv.appendTo($('.wish_show'));
+        function show(){
+            var mydate = new Date();
+            var str = mydate.getHours()+':'; //获取当前小时数(0-23)
+            str+=mydate.getMinutes(); //获取当前分钟数(0-59)
+            return str;
+        }
+        var ran_l = Math.random()*940;
+        var ran_t = Math.random()*170;
+        aDiv.css({'left':ran_l+'px','top':ran_t+'px'});
+    });
+    $('.wish_show').on('click','.newDiv_p4',function(){
+        $(this).parent().remove();
+    });
+    var move=false;//移动标记
+    var _x,_y;//鼠标离控件左上角的相对位置
+    var aa= 0 ;
+    $(".wish_show").on('mousedown','.newDiv',function(e){
+        move=true;
+        aa = $(this).index();
+        _x=e.pageX-parseInt($(this).css("left"));
+        _y=e.pageY-parseInt($(this).css("top"));
+    });
+    $(document).mousemove(function(e){
+        e.preventDefault();
+        if(move){
+            var x=e.pageX-_x;//控件左上角到屏幕左上角的相对位置
+            var y=e.pageY-_y;
+            $(".newDiv").eq(aa).css({"top":y,"left":x});
+        }
+    }).mouseup(function(){
+        move=false;
+    });
 });
